0ed2f5e28851887a8f1939ebc7832f250939b966
Arthur-Diasz <cdp1821@gmail.com>
2024-11-30 17:16:53 -0300

    Ajustes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+AOS.init({
+    duration: 1000, // Duração da animação em milissegundos
+    easing: "ease-in-out", // Tipo de easing para a animação
+    once: false, // Executa a animação apenas uma vez ao rolar a página
+});
+
+
